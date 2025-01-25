ccc9bd9bec633e0dbaf8f1b904374c452b876605
alsdk1676 <alsdk6761@gmail.com>
2025-01-25 23:37:44 +0900

    day04_study

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import LifeCycleContainer from './day03/life-cycle/class/LifeCycleContainer';
 import SideEffect from './day03/life-cycle/function/side-effect/SideEffect';
 import SideEffectMount from './day03/life-cycle/function/side-effect/SideEffectMount';
 import SideEffectTask01 from './day03/life-cycle/function/side-effect/SideEffectTask01';
+import ProductContainer from './day04/life-cycle/function/side-effect/memoization/ProductContainer';
 
 
 function App() {
   return (
     <>
-      <SideEffectTask01/>
+      <ProductContainer/>
     </>
   );
 }
