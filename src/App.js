2cf0477ee2587eb2d1f53d4df53b10650188c199
alsdk1676 <alsdk6761@gmail.com>
2025-01-26 21:43:49 +0900

    day04 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import CheckMessage from './day02/ref/expert/CheckMessage';
-import LifeCycleContainer from './day03/life-cycle/class/LifeCycleContainer';
-import SideEffect from './day03/life-cycle/function/side-effect/SideEffect';
-import SideEffectMount from './day03/life-cycle/function/side-effect/SideEffectMount';
 import SideEffectTask01 from './day03/life-cycle/function/side-effect/SideEffectTask01';
 import ProductContainer from './day04/life-cycle/function/side-effect/memoization/ProductContainer';
-
 
 function App() {
   return (
     <>
-      <ProductContainer/>
+      {/* <LifeCycleContainer /> */}
+      {/* <Count /> */}
+      {/* <SideEffectMount /> */}
+      {/* <SideEffect /> */}
+      {/* <SideEffectTask01 /> */}
+      <ProductContainer />
     </>
   );
 }
