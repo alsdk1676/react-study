4945933b3cc9c486f64dc69f6926ffac0e20161d
alsdk1676 <alsdk6761@gmail.com>
2025-04-18 23:41:10 +0900

    redux 0418

--- src/day06/redux/Counter2.jsx
+++ src/day06/redux/Counter2.jsx
@@ -3,20 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { decrease, increase } from '../../modules/count';
 import useInput from '../hooks/useInput';
 
-const Counter = () => {
-  // store에 값을 가져오는 훅함수 useSelector()
-  // console.log(useSelector((state) => state))
+const Counter2 = () => {
+
   const { number } = useSelector((state) => state)
-  // 액션을 발생시키는 훅 함수 useDispatch()
+
   const dispatch = useDispatch()
 
   const [value, onChangeValue] = useInput("")
-  // const [value, setValue] = useState()
-  // const onChangeValue = (e) => {
-  //   setValue(e.target.value)
-  // }
-  
-  // console.log(value)
+
 
   return (
     <div>
@@ -30,4 +24,4 @@ const Counter = () => {
   );
 };
 
-export default Counter;
+export default Counter2;
---
+++ src/modules/count2.js
@@ -0,0 +1,41 @@
+import { createAction, handleActions } from 'redux-actions'
+
+// type 생성 
+const INCREASE = "count/INCREASE"
+const DECREASE = "count/DECREASE"
+
+// action 생성 (외부 component에서 사용하기 때문에 export로 내보내기)
+export const increase = createAction(INCREASE)
+export const decrease = createAction(DECREASE)
+
+// state 초기화
+const initialState = {
+  number : 0
+}
+
+// reducer 제작
+const count2 = handleActions({
+  [INCREASE] : (state, action) => ({ number : state.number  + parseInt(action.payload) }), 
+  [DECREASE] : (state, action) => ({ number : state.number - parseInt(action.payload) }), 
+}, initialState)
+
+export default count2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
